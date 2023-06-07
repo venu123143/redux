1fde28dee5027507d907ff295f9fd11bbd19afcc
venu reddy <venugopalreddy9493@gmail.com>
2023-06-07 14:55:59 +0530

    redux first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hobbies from './components/Hobbies'
+
+function App() {
+    return (
+        <div>
+            <Hobbies/>
+        </div>
+    )
+}
+
+export default App
